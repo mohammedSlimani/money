29ff6ec8c26556b147b6f4b5ad4eb8b224a70b5f
Mohammed slimani <slimani.med.21@gmail.com>
2019-08-03 00:23:02 +0100

    Usage of the new routes

--- src/App.js
+++ src/App.js
@@ -6,90 +6,29 @@
  * @flow
  */
 
-import React,{ Fragment, Component} from 'react';
-import {
-  View,
-  Button,
-  TextInput
-} from 'react-native';
-
-import firebase from "firebase"
-import config from "./config"
-
-class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      item : "",
-      price:""
-    }
-    this.submit = this.submit.bind(this);
-    this.reset = this.reset.bind(this);
-  }
-
-  componentDidMount(){
-    console.log("mounting");
-    firebase.initializeApp(config.firebaseConfig);
-    console.log(firebase);
-  }
-
-  submit(){
-    console.log(this.state);
-    console.log(`adding the item ${this.state.item} with price ${this.state.price} `);
-    //TODO: Use push instead of set...
-    firebase.database()
-        .ref("items")
-        .push({"item":this.state.item, "price":this.state.price})
-        .then((res)=>console.log(res))
-        .catch(err=>console.log("err",err))
-    this.reset();
-  }
-
-  reset(){
-    console.log("resetting the State");
-    let db = firebase.database()
-    let itemRef = db.ref().child("items");
-    itemRef.orderByChild("item").once("value").then(data=>console.log(data.val()))
-
-    this.setState({
-      item: "",
-      price: ""
-    })
-    this.itemInput.focus();
-  }
-
-  render(){
+import React from 'react';
+import AddItem from './views/AddItem';
+import Stats from './views/Stats';
+import { createStackNavigator, createAppContainer } from "react-navigation";
+/*
+class App extends React.Component {
+  
+  render() {
     return (
-      <View>
-        
-        <View>
-        <TextInput
-          ref={(input) => { this.itemInput = input; }}
-          placeholder="Enter the Item"
-          value={this.state.item}
-          onSubmitEditing={() => { this.priceInput.focus(); }}
-          onChangeText={(item)=>this.setState({item})}
-          autoFocus
-          blurOnSubmit={false}
-        />
-        <TextInput
-          ref={(input) => { this.priceInput = input; }}
-          keyboardType="numeric"
-          value={this.state.price}
-          onChangeText={(price)=>this.setState({price})}
-          defaultValue="0"
-          placeholder="Enter the Price"
-          onSubmitEditing={this.submit}
-        />
-        <Button
-          title="Add Item"
-          onPress={this.submit}
-        />
-        </View>
-      </View>
+      <AddItem/>
     );
   }
 };
+*/
 
+const AppNavigator = createStackNavigator(
+  {
+  Home:AddItem,
+  StatsRT:Stats
+  },
+  {
+    initialRouteName: "Home"
+  }
+)
 
-export default App;
+export default createAppContainer(AppNavigator);
