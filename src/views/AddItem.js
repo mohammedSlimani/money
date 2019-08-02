4dcfdd1979a9b2f7420938ca4f9b8a5e3356dd4b
Mohammed slimani <slimani.med.21@gmail.com>
2019-08-03 00:22:08 +0100

    Refactoring the AddItem View

---
+++ src/views/AddItem.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react'
+import {
+    View,
+    Button,
+    TextInput
+} from 'react-native';
+
+import firebase from "firebase"
+import config from "./../config"
+
+export class AddItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            item: "",
+            price: ""
+        }
+        this.submit = this.submit.bind(this);
+        this.reset = this.reset.bind(this);
+    }
+
+    componentDidMount() {
+        console.log("mounting");
+        firebase.initializeApp(config.firebaseConfig);
+        console.log(firebase);
+    }
+
+    submit() {
+        console.log(this.state);
+        console.log(`adding the item ${this.state.item} with price ${this.state.price} `);
+        //TODO: Use push instead of set...
+        let paylaod = {
+            "item": this.state.item,
+            "price": this.state.price,
+            "date": new Date()
+        }
+        firebase.database()
+            .ref("items")
+            .push(payload)
+            .then((res) => console.log(res))
+            .catch(err => console.log("err", err))
+        this.reset();
+    }
+
+    reset() {
+        console.log("resetting the State");
+        let db = firebase.database()
+        let itemRef = db.ref().child("items");
+        itemRef.orderByChild("item").once("value").then(data => console.log(data.val()))
+
+        this.setState({
+            item: "",
+            price: ""
+        })
+        this.itemInput.focus();
+    }
+
+    render() {
+        return (
+            <View>
+
+                <View>
+                    <TextInput
+                        ref={(input) => { this.itemInput = input; }}
+                        placeholder="Enter the Item"
+                        value={this.state.item}
+                        onSubmitEditing={() => { this.priceInput.focus(); }}
+                        onChangeText={(item) => this.setState({ item })}
+                        autoFocus
+                        blurOnSubmit={false}
+                    />
+                    <TextInput
+                        ref={(input) => { this.priceInput = input; }}
+                        keyboardType="numeric"
+                        value={this.state.price}
+                        onChangeText={(price) => this.setState({ price })}
+                        defaultValue="0"
+                        placeholder="Enter the Price"
+                        onSubmitEditing={this.submit}
+                    />
+                    <Button
+                        title="Add Item"
+                        onPress={this.submit}
+                    />
+                    <Button
+                        title="see stats"
+                        onPress={()=>{this.props.navigation.navigate('StatsRT')}}
+                    />
+                </View>
+            </View>
+        );
+    }
+}
+
+export default AddItem
