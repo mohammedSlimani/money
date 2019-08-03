c1fef098609007fdcb2277374b91cb4d17953a0b
Mohammed slimani <slimani.med.21@gmail.com>
2019-08-03 17:15:39 +0100

    Refactoring

--- src/App.js
+++ src/App.js
@@ -1,12 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React from 'react';
 import AddItem from './views/AddItem';
 import Stats from './views/Stats';
 import { createDrawerNavigator, createAppContainer } from "react-navigation";
