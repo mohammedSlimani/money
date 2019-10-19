69587f10ac324adbbfc2695e74d3bcc8073697a0
Mohammed slimani <slimani.med.21@gmail.com>
2019-10-19 22:29:07 +0100

    General View of the AuthScreen

--- src/screens/Auth/AuthScreen.js
+++ src/screens/Auth/AuthScreen.js
@@ -1,14 +1,20 @@
 import React from 'react'
-import { Container, Text } from 'native-base';
+import { Container, Text, Header, Body, Title, Left, Right, Button } from 'native-base';
 
 class AuthScreen extends React.Component {
 	
 	render() {
 		return (
 			<Container>
-                <Text>
-                    AUTH SCREEN
-                </Text>
+				<Header>
+					<Title>Please Sign in</Title>
+				</Header>
+				<Body>
+					<Text>KEEP TRACK OF YOUR MONEY</Text>
+					<Button >
+						<Text>Login With FaceBook</Text>
+					</Button>
+				</Body>
 			</Container>
 		);
 	}
