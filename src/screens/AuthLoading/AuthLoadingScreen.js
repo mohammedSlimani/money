ed938f163be0c14a8d9f78e8b46495e8a78dde3b
Mohammed slimani <slimani.med.21@gmail.com>
2019-10-19 21:32:26 +0100

    Implementation of AuthLoading Logic

--- src/screens/AuthLoading/AuthLoadingScreen.js
+++ src/screens/AuthLoading/AuthLoadingScreen.js
@@ -1,13 +1,23 @@
-import React from 'react'
-import { Container, Spinner } from 'native-base';
+import React from "react";
+import { Container, Spinner } from "native-base";
+import AsyncStorage  from "@react-native-community/async-storage";
 
 class AuthLoadingScreen extends React.Component {
+	componentDidMount() {
+		this._checkUser();
+	}
+
+	async _checkUser() {
+		const user = await AsyncStorage.getItem("user");
+		this.props.navigation.navigate(user ? "Home" : "Auth");
+	}
+
 	render() {
 		return (
 			<Container>
-                <Spinner/>
+				<Spinner />
 			</Container>
 		);
 	}
 }
-export default AuthLoadingScreen
+export default AuthLoadingScreen;
