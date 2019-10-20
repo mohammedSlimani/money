d72d244148fda9945c1910047712017690566acc
Mohammed slimani <slimani.med.21@gmail.com>
2019-10-20 18:25:03 +0100

    Smoother UX

--- src/screens/Home/AddItem/AddItemScreen.js
+++ src/screens/Home/AddItem/AddItemScreen.js
@@ -22,6 +22,12 @@ export class AddItemScreen extends Component {
 
 	_submit(){
 		console.log('Submitting ', this.state);	
+		this.refs.titleInput._root.clear()
+		this.refs.priceInput._root.clear()
+		this.setState({
+			title:"",
+			price:""
+		})
 	}
 
 	render() {
@@ -44,12 +50,14 @@ export class AddItemScreen extends Component {
 						autoFocus
 						onChangeText={title => this.setState({ title })}
 						onSubmitEditing = { () => this.refs.priceInput._root.focus()}
+						blurOnSubmit={false}
 					/>
 					<Input
 						ref='priceInput'
 						placeholder="price"
 						keyboardType="decimal-pad"
 						onChangeText={price => this.setState({ price })}
+						blurOnSubmit={false}
 						onSubmitEditing={()=> {
 							this._submit();
 							this.refs.titleInput._root.focus();
