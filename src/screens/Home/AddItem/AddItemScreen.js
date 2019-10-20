73cd7e2e467f7422a3df9927d569d34e70754cfc
Mohammed slimani <slimani.med.21@gmail.com>
2019-10-20 02:22:58 +0100

    Adding Inputs to AddingITem Screen

--- src/screens/Home/AddItem/AddItemScreen.js
+++ src/screens/Home/AddItem/AddItemScreen.js
@@ -1,16 +1,57 @@
-import React, { Component } from 'react'
-import { Container, Text } from 'native-base'
+import React, { Component } from "react";
+import {
+	Container,
+	Text,
+	Button,
+	Icon,
+	Left,
+	Body,
+	Header,
+	Title,
+	Input
+} from "native-base";
 
 export class AddItemScreen extends Component {
-    render() {
-        return (
-            <Container>
-                <Text>
-                    ADD ITEM
-                </Text>
-            </Container>
-        )
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			title: "",
+			price: ""
+		};
+	}
+
+	render() {
+		return (
+			<Container>
+				<Header>
+					<Left>
+						<Button onPress={() => this.props.navigation.openDrawer()}>
+							<Icon type="SimpleLineIcons" name="drawer" />
+						</Button>
+					</Left>
+					<Body>
+						<Title>Add Item</Title>
+					</Body>
+				</Header>
+				<Body>
+					<Input
+						placeholder="Item Name"
+						autoFocus="true"
+						onChangeText={title => this.setState({ title })}
+					/>
+					<Input
+						placeholder="price"
+						keyboardType="decimal-pad"
+						onChangeText={price => this.setState({ price })}
+					/>
+				</Body>
+
+				<Button onPress={() => console.log("state", this.state)}>
+					<Text>BO</Text>
+				</Button>
+			</Container>
+		);
+	}
 }
 
-export default AddItemScreen
+export default AddItemScreen;
--- src/screens/Home/HomeNavigation.js
+++ src/screens/Home/HomeNavigation.js
@@ -2,11 +2,14 @@ import { createDrawerNavigator } from "react-navigation";
 import AddItemScreen from "./AddItem/AddItemScreen";
 import StatsScreen from "./Stats/StatsScreen";
 
-const HomeDrawer = createDrawerNavigator(
-    {
-        AddItemScreen : AddItemScreen,
-        StatsScreen : StatsScreen
-    }
-)
+const HomeDrawer = createDrawerNavigator({
+	AddItemScreen: {
+		screen: AddItemScreen
+	},
+
+	StatsScreen: {
+		screen: StatsScreen
+	}
+});
 
 export default HomeDrawer;
